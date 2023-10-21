cdebd133ded64740f207a40e869dff42898245f4
Aleksey <a.gryshchenko@pinta.pro>
2023-10-21 17:13:46 +0300

    add library detect resize + styled top bar pdf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,9 +1,11 @@
 'use client'
-import { Loader2 } from 'lucide-react'
+import { ChevronDown, ChevronDownCircle, Loader2 } from 'lucide-react'
 import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 import { useToast } from './ui/use-toast'
+import { useResizeDetector } from 'react-resize-detector'
+import { Button } from './ui/button'
 
 interface PdfRenderProps {
   url: string
@@ -14,13 +16,19 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 export default function PdfRenderer({ url }: PdfRenderProps) {
   const { toast } = useToast()
 
+  const { width, ref } = useResizeDetector()
+
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
       <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-        <div className='flex items-center gap-1.5'>top bar</div>
+        <div className='flex items-center gap-1.5'>
+          <Button variant={'ghost'} aria-label='previous-page'>
+            <ChevronDown className='h-4 w-4' />
+          </Button>
+        </div>
       </div>
       <div className='flex-1 w-full max-h-screen'>
-        <div>
+        <div ref={ref}>
           <Document
             loading={
               <div className='flex justify-center'>
@@ -37,7 +45,7 @@ export default function PdfRenderer({ url }: PdfRenderProps) {
             file={url}
             className='max-h-full'
           >
-            <Page pageNumber={1} />
+            <Page width={width ? width : 1} pageNumber={1} />
           </Document>
         </div>
       </div>
