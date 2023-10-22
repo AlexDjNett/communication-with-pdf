b69fdc1e45a70b18c9b091b698ee2f22a62fb9f7
Aleksey <a.gryshchenko@pinta.pro>
2023-10-22 11:03:02 +0300

    add page changed input pdf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,11 +1,18 @@
 'use client'
-import { ChevronDown, ChevronDownCircle, Loader2 } from 'lucide-react'
+import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react'
 import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
-import { useToast } from './ui/use-toast'
 import { useResizeDetector } from 'react-resize-detector'
 import { Button } from './ui/button'
+import { useToast } from './ui/use-toast'
+import { Input } from './ui/input'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { cn } from '@/lib/utils'
 
 interface PdfRenderProps {
   url: string
@@ -16,14 +23,74 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 export default function PdfRenderer({ url }: PdfRenderProps) {
   const { toast } = useToast()
 
+  const [numPages, setNumPages] = useState<number>()
+  const [currPage, setCurrPage] = useState<number>(1)
+
+  const CustomPageValidator = z.object({
+    page: z
+      .string()
+      .refine((num) => Number(num) > 0 && Number(num) < numPages!),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<TCustomPageValidator>({
+    defaultValues: {
+      page: '1',
+    },
+    resolver: zodResolver(CustomPageValidator),
+  })
+
+  type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
   const { width, ref } = useResizeDetector()
+
+  const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+    setCurrPage(Number(page))
+    setValue('page', String(page))
+  }
 
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
       <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
         <div className='flex items-center gap-1.5'>
-          <Button variant={'ghost'} aria-label='previous-page'>
+          <Button
+            disabled={currPage <= 1}
+            onClick={() => setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1))}
+            variant={'ghost'}
+            aria-label='previous-page'>
             <ChevronDown className='h-4 w-4' />
+          </Button>
+          <div className='flex items-center gap-1.5'>
+            <Input
+              {...register('page')}
+              className={cn(
+                'w-12 h-8',
+                errors.page && 'focus-visible:ring-red-500'
+              )}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') handleSubmit(handlePageSubmit)()
+              }}
+            />
+            <p className='text-sm text-zinc-700 space-x-1'>
+              <span>/</span>
+              <span>{numPages ?? 'x'}</span>
+            </p>
+          </div>
+
+          <Button
+            disabled={numPages === undefined || currPage === numPages}
+            onClick={() =>
+              setCurrPage((prev) =>
+                prev + 1 > numPages! ? numPages! : prev + 1
+              )
+            }
+            variant={'ghost'}
+            aria-label='next-page'>
+            <ChevronUp className='h-4 w-4' />
           </Button>
         </div>
       </div>
@@ -35,6 +102,7 @@ export default function PdfRenderer({ url }: PdfRenderProps) {
                 <Loader2 className='my-24 h-6 w-6 animate-spin' />
               </div>
             }
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
             onLoadError={() => {
               toast({
                 title: 'Error loading PDF',
@@ -43,9 +111,8 @@ export default function PdfRenderer({ url }: PdfRenderProps) {
               })
             }}
             file={url}
-            className='max-h-full'
-          >
-            <Page width={width ? width : 1} pageNumber={1} />
+            className='max-h-full'>
+            <Page width={width ? width : 1} pageNumber={currPage} />
           </Document>
         </div>
       </div>
